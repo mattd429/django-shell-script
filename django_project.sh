8d1457fc3d14055d4568781ad62c69caccc6cccf
Matthew Brown <mattd429@gmail.com>
2017-11-04 19:06:19 -0400

    Update django_project.sh

--- django_project.sh
+++ django_project.sh
@@ -1,2 +1,26 @@
 # Shell script to create a complete Django project.
 # This script require Python 3.x and pyenv and Django 1.10
+
+# The project contains:
+# Settings config
+# model and form
+# list and detial
+# create, update and delete
+# Admin config
+# Tests
+# Selenium test
+# Manage shell
+
+# Usage:
+# Type the following command, you can change the project name.
+
+# source django_project.sh tutorial
+
+# Colors
+red='tput setaf 1'
+green='tput setaf 2'
+reset='tput sgr0'
+
+PROJECT=${1-tutorial}
+
+echo "${green}>>> The name of the project is '$PROJECT'.${reset}"
