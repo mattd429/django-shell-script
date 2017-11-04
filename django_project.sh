5b254e3007deb79e3e1217358c4717f26d9e6caa
Matthew Brown <mattd429@gmail.com>
2017-11-04 19:49:49 -0400

    Update django_project.sh
    we created so far, we have the colors, which will display red for and error in writing our django app in bash, or green if the script wwas success in creating the file,  first we create our function to have the project name be called tutorial, then the first echo will display what the project name is in our case it is tutorial, we have a echo to create project but if there is not  A list of all double-quoted strings preceded by djangoproject then create the project, and change the dir to djangoproject.  after that we want to create the venv so we do not interact with any current projects going on, then activate the venv, pip install your requirements.txt file, then create a sample of your secret_key and debug mode and allowed_hosts.

--- django_project.sh
+++ django_project.sh
@@ -24,3 +24,33 @@ reset='tput sgr0'
 PROJECT=${1-tutorial}
 
 echo "${green}>>> The name of the project is '$PROJECT'.${reset}"
+
+echo "${green}>>> Creating djangoproject${reset}"
+if [ ! -d djangoproject ]; then mkdir djangoproject; fi
+cd djangoproject
+
+echo "${green}>>> Creating virtualenv${reset}"
+python -m env .env
+echo "${green}>>> .env is created${reset}"
+
+#active
+sleep 2
+echo "${green}>>> activate the .env${reset}"
+source env/bin/activate
+PS1="('basename \"$VIRTUAL_ENV"')\\e[1;34m:/\W\e[00m$ "
+sleep 2
+
+# installdjango
+echo "${green}>>> Installing the Django${reset}"
+pip install -U pip
+pip install django=1.10 dj-database0url django-database-filter django-localflavor django-widget-tweaks python-decouple pytz selenium django-extensions
+pip freeze > requirements.txt
+
+# Create contrib/env-sample
+echo "${green}>>> Creating the contrib/env-sample${reset}"
+mkdir contrib
+cat << EOF > contirb/env-sample
+SECRET_KEY=''
+ALLOWED_HOSTS=127.0.0.1, .localhost
+EOF
+
